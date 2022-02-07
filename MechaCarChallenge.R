54949853e3411a6cf9d4bf6507d212a5b64abd4e
wbelayne21 <wastexmamo@gmail.com>
2022-02-07 13:46:18 -0500

    summary of PSI for each lot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+##### Deliverable 1#####
+
 # import package
 library(dplyr)
 
@@ -9,3 +11,21 @@ lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,
 
 #Summary() to determine p-value and r-squared 
 summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCar))
+
+
+#### Deliverable 2 ####
+
+# import and read
+SusCoil<-read.csv("Suspension_Coil.csv")
+
+# import stat package
+library(stats)
+
+# summarize all PSI
+total_summary <- SusCoil %>%summarise(Mean = mean(PSI), Median = median(PSI), 
+                            Variance = var(PSI), SD = sd(PSI), .groups = "keep")
+
+# summarize PSI for each lot 
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>%
+  summarise(Mean = mean(PSI), Median = median(PSI), 
+            Variance = var(PSI), SD = sd(PSI), .groups = "keep")
